2d5a9393aafbaf950ee6644e5698094063575ecf
prapul <prapulpoonacha@gmail.com>
2016-12-20 21:48:47 +0530

    print name

---
+++ AndroidSession2_Assignment1.java
@@ -0,0 +1,14 @@
+package prapul;
+
+public class AndroidSession2_Assignment1 {
+
+	public static void main(String[] args) {
+		int i = 1;
+		do{
+			System.out.println("Prapul");
+			i++;
+		}
+		while(i<2);
+	}
+
+}
